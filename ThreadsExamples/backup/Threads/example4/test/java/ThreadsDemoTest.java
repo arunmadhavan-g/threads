357c41fef1fa176727833caae766f2f64f380c0d
Arun Madhavan <arunmadhavan.g@gmail.com>
2016-09-01 05:40:41 +0530

    changes to include synchronization and example 4

---
+++ ThreadsExamples/backup/Threads/example4/test/java/ThreadsDemoTest.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+import org.junit.Test;
+
+import passvalue.ComputationThread;
+import simple.ExtendsThread;
+import simple.ImplementsRunnable;
+
+public class ThreadsDemoTest {
+
+	@Test
+	public void extendsThread() throws Exception {
+		System.out.println("Entering Test Thread");
+		ExtendsThread thread = new ExtendsThread();
+		thread.start();
+		System.out.println("Exiting Test Thread");
+	}
+	
+	@Test
+	public void implementsRunnable() throws Exception {
+		System.out.println("Entering Test Thread");
+		Thread runnableThread = new Thread(new ImplementsRunnable());
+		runnableThread.start();
+		System.out.println("Exiting Test Thread");
+	}
+	
+	@Test
+	public void passingValueToThread() throws Exception {
+		System.out.println("Entering Test Thread");
+		Random random  = new Random();
+		for(int i=0;i<5;i++){
+			int loopNumber = random.nextInt(5)+1;
+			System.out.println(String.format("For loop %d, loop number passed is %d", i, loopNumber));
+			ComputationThread compThread = new ComputationThread(loopNumber);
+			compThread.start();
+		}
+		System.out.println("Exiting Test Thread");
+	}
+	
+	
+}
---
+++ ThreadsExamples/src/main/java/BankThread.java
@@ -0,0 +1,29 @@
+
+public class BankThread extends Thread {
+
+	private Account account;
+	private String operation;
+	private Double amount;
+
+	public BankThread(Account account, String operation, Double amount) {
+		this.account = account;
+		this.operation = operation;
+		this.amount = amount;
+	}
+
+	@Override
+	public void run() {
+		synchronized (account) {
+			System.out.println(String.format("Operating on %s account and doing a %s or amount %.2f", account.getName(),
+					operation, amount));
+			switch (this.operation) {
+				case "deposit": account.deposit(amount); break;
+				case "withdraw": account.withdraw(amount); break;
+				case "enquire": account.enquire(); break;
+				default: account.enquire();
+			}
+		}
+		super.run();
+	}
+
+}
